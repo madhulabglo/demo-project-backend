37750e7e3b6758cc4a46750c35cb63ca4266266c
user <madhumathi.labglo@gmail.com>
2023-04-25 17:06:07 +0530

    add babel

--- src/index.js
+++ src/index.js
@@ -27,6 +27,18 @@ mongoose.connect(DB).then(()=>{
     console.log("connection failed")
 })
 
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+
 app.get("/",(req,res)=>{
     res.send("server Running")
 })
